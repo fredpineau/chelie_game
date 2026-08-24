148cf85c0a56f79ea471aab728480d5992de82b6
fredpineau <97562669+fredpineau@users.noreply.github.com>
2026-08-24 21:29:20 +0200

    chore: add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.fredpineau.cheliegame",
+  appName: "Chelie Game",
+  webDir: "dist",
+};
+
+export default config;
