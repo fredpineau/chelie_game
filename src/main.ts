150bfefcaba202cc70ce220dafb89232ae0ef089
fredpineau <97562669+fredpineau@users.noreply.github.com>
2026-08-25 19:54:01 +0200

    style: reduce placed plant size

--- src/main.ts
+++ src/main.ts
@@ -1057,9 +1057,9 @@ class DefenseScene extends Phaser.Scene {
     this.energy -= definition.cost;
     const towerBody = this.add.container(towerX, towerY);
 
-    const base = this.add.circle(0, 9, 27, TEMP_LEVEL_COLORS[0]).setStrokeStyle(3, 0x4d7c0f, 0.85);
-    const plant = this.createPlantVisual(selectedKind, definition.color);
-    const levelBadge = this.add.text(21, 21, "", {
+    const base = this.add.circle(0, 8, 23, TEMP_LEVEL_COLORS[0]).setStrokeStyle(3, 0x4d7c0f, 0.85);
+    const plant = this.createPlantVisual(selectedKind, definition.color).setScale(0.78);
+    const levelBadge = this.add.text(18, 18, "", {
       fontFamily: "Arial",
       fontSize: "14px",
       color: "#ffffff",
@@ -1564,7 +1564,7 @@ class DefenseScene extends Phaser.Scene {
     base.setStrokeStyle(tower.level >= 5 ? 5 : 3, tower.level >= 5 ? 0xf4d35e : definition.color, 0.95);
 
     const plant = tower.body.getAt(1) as Phaser.GameObjects.Container;
-    plant.setAlpha(1).setScale(1 + (tower.level - 1) * 0.07);
+    plant.setAlpha(1).setScale(0.78 + (tower.level - 1) * 0.055);
     this.evolveTowerVisual(tower);
     this.createUpgradePulse(tower, definition.color);
     const nextCost = tower.level < MAX_TOWER_LEVEL ? UPGRADE_COSTS[tower.level] : null;
