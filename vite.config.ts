84cbb8842b9882900f14f08fe3759a75a3e46359
fredpineau <97562669+fredpineau@users.noreply.github.com>
2026-08-28 18:28:00 +0200

    Preserve accessible infinite mode balance

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig, type UserConfig } from "vite";
 import baseConfig from "./vite.base.config";
 import { endgameProgression } from "./endgameProgressionPlugin";
 import { endgameDropProgress } from "./endgameDropProgressPlugin";
+import { preserveAccessibleInfiniteMode } from "./infiniteDifficultyPlugin";
 
 const config = baseConfig as UserConfig;
 
 export default defineConfig({
   ...config,
-  plugins: [...(config.plugins ?? []), endgameProgression(), endgameDropProgress()],
+  plugins: [
+    ...(config.plugins ?? []),
+    endgameProgression(),
+    endgameDropProgress(),
+    preserveAccessibleInfiniteMode(),
+  ],
 });
