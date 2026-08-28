2f7b0e1d272a9a4069ee1d7b4ce921d2f05813db
fredpineau <97562669+fredpineau@users.noreply.github.com>
2026-08-28 18:26:58 +0200

    Wire accurate endgame drop progress

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig, type UserConfig } from "vite";
 import baseConfig from "./vite.base.config";
 import { endgameProgression } from "./endgameProgressionPlugin";
+import { endgameDropProgress } from "./endgameDropProgressPlugin";
 
 const config = baseConfig as UserConfig;
 
 export default defineConfig({
   ...config,
-  plugins: [...(config.plugins ?? []), endgameProgression()],
+  plugins: [...(config.plugins ?? []), endgameProgression(), endgameDropProgress()],
 });
