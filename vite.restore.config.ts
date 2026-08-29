7355e9f55658da6853a044b8447b2e32f6f0aaa5
fredpineau <97562669+fredpineau@users.noreply.github.com>
2026-08-29 23:34:31 +0200

    restore: enable accurate endgame drop progress

--- vite.restore.config.ts
+++ vite.restore.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import baseConfig from "./vite.config";
 import { clearMasteryDisplay } from "./masteryDisplayPlugin";
+import { endgameDropProgress } from "./endgameDropProgressPlugin";
 
 export default defineConfig({
   ...baseConfig,
   plugins: [
     ...(baseConfig.plugins ?? []),
+    endgameDropProgress(),
     clearMasteryDisplay(),
   ],
 });
