ad30bf49a2b5ff297b365d47954062755fb2d849
fredpineau <97562669+fredpineau@users.noreply.github.com>
2026-08-29 23:57:15 +0200

    restore: enable endgame progression

--- vite.restore.config.ts
+++ vite.restore.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import baseConfig from "./vite.config";
 import { clearMasteryDisplay } from "./masteryDisplayPlugin";
 import { endgameDropProgress } from "./endgameDropProgressPlugin";
+import { endgameProgression } from "./endgameProgressionPlugin";
 
 export default defineConfig({
   ...baseConfig,
   plugins: [
     ...(baseConfig.plugins ?? []),
+    endgameProgression(),
     endgameDropProgress(),
     clearMasteryDisplay(),
   ],
