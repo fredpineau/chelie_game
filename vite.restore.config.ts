927bacc4a71d674b8a6d6e2d99386402d8425429
fredpineau <97562669+fredpineau@users.noreply.github.com>
2026-09-14 15:44:29 +0200

    test: enable drop seed on refund preview

--- vite.restore.config.ts
+++ vite.restore.config.ts
@@ -12,6 +12,7 @@ import { endgameLevelsFallback } from "./endgameLevelsFallbackPlugin";
 import { endgameProgression } from "./endgameProgressionPlugin";
 import { greenhouseMapButton } from "./greenhouseMapButtonPlugin";
 import { pathfindingStability } from "./pathfindingStabilityPlugin";
+import { testDropSeed } from "./testDropSeedPlugin";
 import { wateringGuideMastery } from "./wateringGuideMasteryPlugin";
 import { waveSpeedControl } from "./waveSpeedControlPlugin";
 import { worldSelectionSpacing } from "./worldSelectionSpacingPlugin";
@@ -29,6 +30,7 @@ export default defineConfig({
     endgameDropProgress(),
     clearMasteryDisplay(),
     greenhouseMapButton(),
+    testDropSeed(),
     wateringGuideMastery(),
     worldSelectionSpacing(),
     worldVisualIdentity(),
